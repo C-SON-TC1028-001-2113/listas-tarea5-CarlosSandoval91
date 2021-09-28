c8ab9434f8072bc435513ba191a0539c8941fe28
CarlosSandoval91 <A01252494@itesm.mx>
2021-09-28 04:25:31 +0000

    atrea

--- assignments/08SinDuplicados/src/exercise.py
+++ assignments/08SinDuplicados/src/exercise.py
@@ -1,6 +1,23 @@
+
 def main():
     #escribe tu código abajo de esta línea
+    x= int(input())
+    i=0
+    listas=[]
+    if x>0:
+        while i<x:
+            u=str(input())
+            listas.append(u)
+            i=i+1
+        print (listas)
+        yu=[]
+        for element in listas:
+            if element not in yu:
+                yu.append(element)
+        print (yu)
 
+    else:
+        print('Error')    
 
     pass
 
